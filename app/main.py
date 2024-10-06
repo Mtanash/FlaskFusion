120d336fc8911c042047c6fa71445ac5d9a1964f
Mtanash <tanashkoo@gmail.com>
2024-10-06 14:39:08 +0300

    add static serving point

--- app/main.py
+++ app/main.py
@@ -1,21 +1,27 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from app.routes.csv_routes import csv_routes
 from app.routes.images_routes import images_routes
 from app.routes.text_routes import text_routes
 from dotenv import load_dotenv
 from flask_cors import CORS
+import os
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# config
-app.config["UPLOAD_FOLDER"] = "uploads"
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
 
 app.register_blueprint(csv_routes)
 app.register_blueprint(images_routes)
 app.register_blueprint(text_routes)
+
+
+# serve the uploaded files
+@app.route("/uploads/<path:name>", methods=["GET"])
+def serve_file(name):
+    return send_from_directory(UPLOAD_FOLDER, name)
 
 
 @app.route("/")
